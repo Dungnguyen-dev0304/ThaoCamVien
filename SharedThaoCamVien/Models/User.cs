8f29c8bf9f6611f6d5d95eb660eaa12504e65f9a
tfduongw <nttd482005@gmail.com>
2026-03-18 20:05:23 +0700

    update UserModel

--- SharedThaoCamVien/Models/User.cs
+++ SharedThaoCamVien/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SharedThaoCamVien.Models;
 
@@ -7,11 +8,17 @@ public partial class User
 {
     public int UserId { get; set; }
 
-    public string? Email { get; set; }
+    [Required]
+    public string Email { get; set; }
 
-    public string? DisplayName { get; set; }
+    [Required]
+    public string DisplayName { get; set; }
 
-    public DateTime? CreatedAt { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    [StringLength(50)]
+    public String Password { get; set; }
 
     public virtual ICollection<PoiVisitHistory> PoiVisitHistories { get; set; } = new List<PoiVisitHistory>();
 
